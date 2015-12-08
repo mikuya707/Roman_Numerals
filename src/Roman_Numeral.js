846d08fd3be6d793f18e99c31a3a0c2f03926a72
Karen Mao <karen.mao707@gmail.com>
2015-12-08 12:51:07 -0500

    Convert roman to numerals

--- src/Roman_Numeral.js
+++ src/Roman_Numeral.js
@@ -52,3 +52,25 @@ function ones(num, deg){
     }
 }
 
+/**Convert Romans to Numerals*/
+function romanToNumeral(roman){
+    var romans = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    return [].reduce.call(roman, function(sum, _, i){
+        var current = romans[roman[i]],
+            prev = romans[roman[i-1]],
+            next = romans[roman[i+1]] || 0;
+
+        if (prev && prev < current) return sum;
+
+        return sum + (current < next ? next - current : current);
+    }, 0);
+}
